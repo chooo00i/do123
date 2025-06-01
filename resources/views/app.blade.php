5dba5b676dc51c7ae227db6262c57696e7455b6f
chooo00i <dkfks961025@gmail.com>
2025-06-01 19:19:27 +0900

    chage title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Do123</title>
         @routes
         @vite('resources/js/app.js')
         @inertiaHead
