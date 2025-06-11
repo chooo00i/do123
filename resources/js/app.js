fbd2fd4ebe5247e5a7a48ed8c925c87f9280400b
chooo00i <dkfks961025@gmail.com>
2025-06-12 02:01:24 +0900

    Enable dynamic page imports to fix warning

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,19 +5,23 @@ import MainLayout from "@/Layouts/MainLayout.vue";
 import "../css/app.css";
 import "flowbite";
 import "emoji-picker-element";
-import '@toast-ui/calendar/dist/toastui-calendar.min.css'
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+// import '@toast-ui/calendar/dist/toastui-calendar.min.css'
 
 createInertiaApp({
     resolve: async (name) => {
-        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true })
-        const page = await pages[`./Pages/${name}.vue`]
-        page.default.layout = page.default.layout || MainLayout
-        return page
+        const page = await resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        );
+        page.default.layout = page.default.layout || MainLayout;
+        return page;
     },
+
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .mount(el)
+            .mount(el);
     },
 });
